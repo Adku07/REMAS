5ad9ed25451a63ac2bc37498e6435a15626abb85
Adku07 <110002242+Adku07@users.noreply.github.com>
2024-06-05 14:29:54 +0700

    Update ExampleTest.php

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,8 +4,12 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    public function testExample()
+    public function testDatabaseConnection()
     {
-        $this->assertTrue(true);
+        // Lakukan koneksi ke database
+        $koneksi = mysqli_connect('localhost', 'root', '', 'pelatihan');
+
+        // Pastikan koneksi berhasil
+        $this->assertNotFalse($koneksi, 'Gagal terkoneksi ke database: ' . mysqli_connect_error());
     }
 }
